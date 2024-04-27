162de1afd4a55b6cb5773b083e348a44fd1a38b1
Andrew Chan <chan.andrew1995@gmail.com>
2024-04-26 23:17:00 -0400

    got a basic pie graph working

--- src/Components/PieChart.js
+++ src/Components/PieChart.js
@@ -1,13 +1,13 @@
 import * as d3 from 'd3';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 const PieChart = () => {
     const userData = useSelector(state => state.user.userData);
     const [colorPicker, updateColorPicker] = useState([]);
+    const svgElement = useRef();
 
     useEffect(() => {
-        console.log(userData);
         let listOfBudgetItems = userData.budgetItems;
         let newListOfColors = [];
 
@@ -15,7 +15,29 @@ const PieChart = () => {
             newListOfColors.push("rgb(" + calculateRandomNumber(256) + ", " + calculateRandomNumber(256) + ", " + calculateRandomNumber(256) + ")");
         }
 
-        //console.log(newListOfColors);
+        let svg = d3.select(svgElement.current);
+
+        svg.selectAll("*").remove(); //remove everything inside the svg
+
+        let width = svg.attr("width");
+        let height = svg.attr("height");
+        let radius = Math.min(width, height)/2
+        let g = svg.append("g").attr("transform", "translate(" + width/2 + ", " + height/2 + ")");
+
+        let color = d3.scaleOrdinal(newListOfColors);
+
+        let data = [{"activity": "gaming", "percent": 40},{"activity": "sleeping", "percent": 45}];
+
+        let pie = d3.pie().value((d) => d.percent);
+        let arc = d3.arc().outerRadius(radius).innerRadius(30);
+        let label = d3.arc().outerRadius(radius).innerRadius(30);
+
+        let arcs = g.selectAll(".arc").data(pie(data)).enter().append("g").attr("class","arc");
+
+        arcs.append("path").attr("d", arc).attr("fill", (d) => color(d.data.activity));
+
+        arcs.append("text").attr("transform", (d) => `translate(${arc.centroid(d)})`).attr("text-anchor", "middle").text((d) => d.data.activity);
+
     },[userData]);
 
     const calculateRandomNumber = (max) => {
@@ -23,9 +45,7 @@ const PieChart = () => {
     }
 
     return(
-        <svg className="w-full">
-            <circle cx="0" cy="0" r="20"></circle>
-        </svg>
+        <svg ref={svgElement} width="200" height="200"></svg>
     )
 }
 
